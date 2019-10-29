edd1dc3fd91c6a712063ebbb40dc769f788dee56
Dgreat21 <dencool1108@icloud.com>
2019-10-29 17:12:28 +0300

    PHP

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width-device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/fonts.css">
+	<link rel="stylesheet" type="text/css" href="css/media.css">
+	<title>SIP</title>
+</head>
+<body>
+	<header>
+		<div class="wrapper">
+			<div class="menu">
+				<div class="menu_left">
+					<nav>
+						<ul class="navigation">
+							<li><a href="index.html">Запись к врачу</a></li>
+							<li><a href="hospitals.html">Учреждения</a></li>
+							<li><a href="reference.html">Справки</a></li>
+							<li><a href="insurance.html">Страховка</a></li>
+							<li><a href="prevention.html">Профилактика</a></li>
+						</ul>
+					</nav>
+				</div>
+				<div class="menu_right">
+					<button class="login">Войти</button>
+				</div>
+			</div>
+		</div>
+	</header>
+	<section>
+		<div class="wrapper">
+		<div class="content">
+			<h1>Записаться на прием к врачу</h1>
+			<div class="">
+				<form class ="appointment" action="">
+					<label for="">Номер полиса ОМС</label>
+					<div class="oms">
+						<input type="tel" placeholder="Например, 2362 7543 2365 5486">
+						<button class="submit">
+							<span>Продолжить</span>
+						</button>
+					</div>
+				</form>
+				<div class="about_oms">
+				<a href="#">Как записаться?</a>
+				<a href="#">Что такое полис ОМС?</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	</section>
+	<footer>
+		<div class="wrapper">
+			<div class="footer">
+				<div class="about">
+					<h2>Сервисы</h2>
+					<a href="#">Запись к врачу</a>
+					<a href="#">Учреждения</a>
+					<a href="#">Справки</a>
+					<a href="#">Страховка</a>
+					<a href="#">Профилактика</a>
+				</div>
+				<div class="about">
+					<h2>Компания</h2>
+					<a href="#">О компании</a>
+					<a href="#">Команда</a>
+					<a href="#">Парнеры</a>
+					<a href="#">Отзывы</a>
+					<a href="#">Продукты</a>
+				</div>
+				<div class="about">
+					<h2>Полезно</h2>
+					<a href="#">Личный кабинет</a>
+					<a href="#">Помощь</a>
+					<a href="#">Условия</a>
+				</div>
+				<div class="about">
+					<h2>Контакты</h2>
+					<a href="tel:+7 (988) 33 22 11">Позвоните нам</a>
+					<a href="mailto:blablabla@gmail.com">blablabla@gmail.com</a>
+					<a href="https://yandex.ru/maps/213/moscow/house/prospekt_vernadskogo_39/37.505894,55.675544/?ll=37.505894%2C55.675545&z=16" target="_blank">Москва, ул. Пушкина 5,
+					<br> 10 этаж, офис 13</a>
+				</div>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
---
+++ reference.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width-device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/fonts.css">
+	<link rel="stylesheet" type="text/css" href="css/media.css">
+	<title>SIP</title>
+</head>
+<body>
+<header>
+	<div class="wrapper">
+		<div class="menu">
+			<div class="menu_left">
+				<nav>
+					<ul class="navigation">
+						<li><a href="index.html">Запись к врачу</a></li>
+						<li><a href="hospitals.html">Учреждения</a></li>
+						<li><a href="reference.html">Справки</a></li>
+						<li><a href="insurance.html">Страховка</a></li>
+						<li><a href="prevention.html">Профилактика</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</div>
+</header>
+<footer>
+	<div class="wrapper">
+		<div class="footer">
+			<div class="about">
+				<h2>Сервисы</h2>
+				<a href="#">Запись к врачу</a>
+				<a href="#">Учреждения</a>
+				<a href="#">Справки</a>
+				<a href="#">Страховка</a>
+				<a href="#">Профилактика</a>
+			</div>
+			<div class="about">
+				<h2>Компания</h2>
+				<a href="#">О компании</a>
+				<a href="#">Команда</a>
+				<a href="#">Парнеры</a>
+				<a href="#">Отзывы</a>
+				<a href="#">Продукты</a>
+			</div>
+			<div class="about">
+				<h2>Полезно</h2>
+				<a href="#">Личный кабинет</a>
+				<a href="#">Помощь</a>
+				<a href="#">Условия</a>
+			</div>
+			<div class="about">
+				<h2>Контакты</h2>
+				<a href="tel:+7 (988) 33 22 11">Позвоните нам</a>
+				<a href="mailto:blablabla@gmail.com">blablabla@gmail.com</a>
+				<a href="https://yandex.ru/maps/213/moscow/house/prospekt_vernadskogo_39/37.505894,55.675544/?ll=37.505894%2C55.675545&z=16" target="_blank">Москва, ул. Пушкина 5,
+					<br> 10 этаж, офис 13</a>
+			</div>
+		</div>
+	</div>
+</footer>
+</body>
+</html>
