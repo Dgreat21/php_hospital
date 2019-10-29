729693f3db669eb549b039f1d4d8604186931771
Dgreat21 <dencool1108@icloud.com>
2019-10-29 18:52:50 +0300

    add header

--- index.php
+++ index.php
@@ -1,91 +1,98 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width-device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="css/reset.css">
-	<link rel="stylesheet" type="text/css" href="css/main.css">
-	<link rel="stylesheet" type="text/css" href="css/fonts.css">
-	<link rel="stylesheet" type="text/css" href="css/media.css">
-	<title>SIP</title>
-</head>
-<body>
-	<header>
-		<div class="wrapper">
-			<div class="menu">
-				<div class="menu_left">
-					<nav>
-						<ul class="navigation">
-							<li><a href="index.html">Запись к врачу</a></li>
-							<li><a href="hospitals.html">Учреждения</a></li>
-							<li><a href="reference.html">Справки</a></li>
-							<li><a href="insurance.html">Страховка</a></li>
-							<li><a href="prevention.html">Профилактика</a></li>
-						</ul>
-					</nav>
+<?php
+include_once "header.html"
+?>
+<section>
+<div class="wrapper">
+	<div class="content">
+		<div class="blockstyle">
+			<div class="line">
+				<div class="layer1">
+					<p>ДЕТСКАЯ ГОРОДСКАЯ ПОЛИКЛИНИКА</p>
+					<p>ГБУЗ г. Москвы "Детская городская поликлиника № 133 ДЗМ"</p>
+					<p>г. Москва, Смольная улица, д. 55</p>
 				</div>
-				<div class="menu_right">
-					<button class="login">Войти</button>
-				</div>
+					<a href="#" class="__r right">Узнать расписание</a>
+			</div>
+			<div class="layer1 top">
+				<a href="tel:+74991953005">+7(499) 195-30-05</a>
 			</div>
 		</div>
-	</header>
-	<section>
-		<div class="wrapper">
-		<div class="content">
-			<h1>Записаться на прием к врачу</h1>
-			<div class="">
-				<form class ="appointment" action="">
-					<label for="">Номер полиса ОМС</label>
-					<div class="oms">
-						<input type="tel" placeholder="Например, 2362 7543 2365 5486">
-						<button class="submit">
-							<span>Продолжить</span>
-						</button>
-					</div>
-				</form>
-				<div class="about_oms">
-				<a href="#">Как записаться?</a>
-				<a href="#">Что такое полис ОМС?</a>
+		<div class="blockstyle">
+			<div class="line">
+				<div class="layer1">
+					<p>НАРКОЛОГИЧЕСКИЙ ДИСПАНСЕР</p>
+					<p>ГКУЗ МНПЦ Н филиал № 2 ДЗМ (НД №2)</p>
+					<p>г. Москва, улица Куусинена, д. 4, корп. 3</p>
 				</div>
+					<a href="#" class="__r right">Узнать расписание</a>
+			</div>
+			<div class="layer1 top">
+				<a href="tel:+74991953005">+7(499) 352-64-25</a>
+			</div>
+		</div>
+		<div class="blockstyle">
+			<div class="line">
+				<div class="layer1">
+					<p>СТОМАТОЛОГИЧЕСКАЯ ПОЛИКЛИНИКА</p>
+					<p>ГАУЗ "СП № 49 ДЗМ"</p>
+					<p>г. Москва, Солнечногорский проезд, д. 9</p>
+				</div>
+					<a href="#" class="__r right">Узнать расписание</a>
+			</div>
+			<div class="layer1 top">
+				<a href="tel:+74991953005">+7(499) 132-01-11</a>
+			</div>
+		</div>
+		<div class="blockstyle">
+			<div class="line">
+				<div class="layer1">
+					<p>ДЕТСКАЯ ГОРОДСКАЯ ПОЛИКЛИНИКА</p>
+					<p>ГБУЗ г. Москвы "Детская городская поликлиника № 94 ДЗМ"</p>
+					<p>г. Москва, Вишневая улица, д. 20, корп. 2</p>
+				</div>
+					<a href="#" class="__r right">Узнать расписание</a>
+			</div>
+			<div class="layer1 top">
+			<a href="tel:+74991953005">+7(499) 463-37-43</a>
 			</div>
 		</div>
 	</div>
-	</section>
-	<footer>
-		<div class="wrapper">
-			<div class="footer">
-				<div class="about">
-					<h2>Сервисы</h2>
-					<a href="#">Запись к врачу</a>
-					<a href="#">Учреждения</a>
-					<a href="#">Справки</a>
-					<a href="#">Страховка</a>
-					<a href="#">Профилактика</a>
-				</div>
-				<div class="about">
-					<h2>Компания</h2>
-					<a href="#">О компании</a>
-					<a href="#">Команда</a>
-					<a href="#">Парнеры</a>
-					<a href="#">Отзывы</a>
-					<a href="#">Продукты</a>
-				</div>
-				<div class="about">
-					<h2>Полезно</h2>
-					<a href="#">Личный кабинет</a>
-					<a href="#">Помощь</a>
-					<a href="#">Условия</a>
-				</div>
-				<div class="about">
-					<h2>Контакты</h2>
-					<a href="tel:+7 (988) 33 22 11">Позвоните нам</a>
-					<a href="mailto:blablabla@gmail.com">blablabla@gmail.com</a>
-					<a href="https://yandex.ru/maps/213/moscow/house/prospekt_vernadskogo_39/37.505894,55.675544/?ll=37.505894%2C55.675545&z=16" target="_blank">Москва, ул. Пушкина 5,
+</div>
+</section>
+<footer>
+	<div class="wrapper">
+		<div class="footer">
+			<div class="about">
+				<h2>Сервисы</h2>
+				<a href="#">Запись к врачу</a>
+				<a href="#">Учреждения</a>
+				<a href="#">Справки</a>
+				<a href="#">Страховка</a>
+				<a href="#">Профилактика</a>
+			</div>
+			<div class="about">
+				<h2>Компания</h2>
+				<a href="#">О компании</a>
+				<a href="#">Команда</a>
+				<a href="#">Парнеры</a>
+				<a href="#">Отзывы</a>
+				<a href="#">Продукты</a>
+			</div>
+			<div class="about">
+				<h2>Полезно</h2>
+				<a href="#">Личный кабинет</a>
+				<a href="#">Помощь</a>
+				<a href="#">Условия</a>
+			</div>
+			<div class="about">
+				<h2>Контакты</h2>
+				<a href="tel:+7 (988) 33 22 11">Позвоните нам</a>
+				<a href="mailto:blablabla@gmail.com">blablabla@gmail.com</a>
+				<a href="https://yandex.ru/maps/213/moscow/house/prospekt_vernadskogo_39/37.505894,55.675544/?ll=37.505894%2C55.675545&z=16" target="_blank">Москва, ул. Пушкина 5,
 					<br> 10 этаж, офис 13</a>
-				</div>
 			</div>
 		</div>
-	</footer>
+	</div>
+</footer>
 </body>
 </html>
---
+++ start.sh
@@ -0,0 +1,1 @@
+ cd ~/hospital && php -S 0.0.0.0:8001
