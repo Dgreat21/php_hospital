ffe65311f2d4d4b10d9e94f539b22cee4308beb4
Mark Shamray <endeymus@gmail.com>
2019-11-12 21:52:34 +0300

    Чуть подкорректировал

--- js/registration.js
+++ js/registration.js
@@ -22,7 +22,7 @@ function add_market_meta(){
         "<input id='submit' type='submit' value='Войти'>" +
         "<p id='haveno_account'>У вас нет Аккаунта?<a href='#'>Зарегистрироваться</a></p>" +
         //Вот поле я добавил
-        "<input type='hidden' name='form' value='Sing in'>" + "</fieldset>" +
+        "<input type='hidden' name='form' value='Sign in'>" + "</fieldset>" +
         "</form>";
 
     document.getElementsByClassName("meta-div")[0].appendChild(background);
