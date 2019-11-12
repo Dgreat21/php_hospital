5844fcc0bc92c349186a646deb0113af9ae7b362
dgreat <dencool1108@icloud.com>
2019-11-12 21:09:38 +0300

    full registration

--- handler.php
+++ handler.php
@@ -29,41 +29,44 @@
 		],
 	];
 
+
 $reg_polis = $_POST['polis_reg'];
 $passconf1 = $_POST['password'];
 $passconf2 = $_POST['password_confirm'];
 $email = $_POST['soap'];
 $name = $_POST['name'];
 $surname = $_POST['surname'];
+if ($_POST['Sign'] == '')
+    {$_POST['Sign'] = "Sign in";}
 
 
 function check_p($reg_auth) {
 	//var_dump($reg_auth);
 	if (!$reg_auth) {//0--sign up
 		if ($_POST['polis_reg'] == '')
-			$status_err['no']['polis_reg'] = true; else    $status_err['no']['polis_reg'] = false;//
-		if ($_POST['soap'] == '')
-			$status_err['no']['email'] = true; else    $status_err['no']['email'] = false;//
-		if ($_POST['password'] == '')
-			$status_err['no']['pass-'] = true; else    $status_err['no']['pass-'] = false;//
-		if ($_POST['password_confirm'] == '')
+			$status_err['no']['polis_reg'] = true; else    $status_err['no']['polis_reg'] = false;
+		if ($_POST['email'] == '')
+			$status_err['no']['email'] = true; else    $status_err['no']['email'] = false;
+		if ($_POST['pass-'] == '')
+			$status_err['no']['pass-'] = true; else    $status_err['no']['pass-'] = false;
+		if ($_POST['pass+'] == '')
 			$status_err['no']['pass+'] = true; else    $status_err['no']['pass+'] = false;
-		if ($_POST['password_confirm'] == '')
+		if ($_POST['name'] == '')
 			$status_err['no']['name'] = true; else    $status_err['no']['name'] = false;
-		if ($_POST['password_confirm'] == '')
-			$status_err['no']['surname'] = true; else    $status_err['no']['surname'] = false;//
-		$status_err['check'] = ($status_err['no']['polis_reg'] && $status_err['no']['email'] && $status_err['no']['pass-'] && $status_err['no']['pass+'] && $status_err['no']['name'] && $status_err['no']['surname']);
+		if ($_POST['surname'] == '')
+			$status_err['no']['surname'] = true; else    $status_err['no']['surname'] = false;
+		$status_err['check'] = !($status_err['no']['polis_reg'] && $status_err['no']['email'] && $status_err['no']['pass-'] && $status_err['no']['pass+'] && $status_err['no']['name'] && $status_err['no']['surname']);
 
 		return $status_err;
 	} else if ($reg_auth) {
-		if ($_POST['polis'] == '')
+		if ($_POST['polis_reg'] == '')
 			$status_err['no']['name'] = true; else $status_err['no']['name'] = false;
 		if ($_POST['pass'] == '')
 			$status_err['pass'] = true; else $status_err['no']['pass'] = false;
-		var_dump($_POST);
-		$status_err['no']['name'] = ($_POST['polis'] == '') ? (false) : (true);
+//		var_dump($_POST);
+		$status_err['no']['name'] = ($_POST['polis_reg'] == '') ? (false) : (true);
 		$status_err['no']['pass'] = ($_POST['password'] == '') ? (false) : (true);
-		$status_err['check'] = ($status_err['no']['name'] && $status_err['no']['pass']);
+		$status_err['check'] = !($status_err['no']['name'] && $status_err['no']['pass']);
 
 		return $status_err;
 		//        }
@@ -82,15 +85,15 @@ function check_p($reg_auth) {
 
 	if (($_POST['Sign'] != NULL)) {
 
-		if ($_POST['action'] == 'Sign up') {
+		if ($_POST['Sign'] == 'Sign up') {
 			//форма регистрации
 			$status_err['no'] = check_p(0);
-			//var_dump($status_err);
+
 			if ($status_err['no']['check']) {
 				$reg_polis = $_POST['polis_reg'];
 				$passconf1 = $_POST['password'];
 				$passconf2 = $_POST['password_confirm'];
-				$email = $_POST['soap'];
+				$email = $_POST['email'];
 				$name = $_POST['name'];
 				$surname = $_POST['surname'];
 
@@ -138,7 +141,7 @@ function check_p($reg_auth) {
 				}
 			}
 		}
-		else if($_POST['action'] == 'Sign in'){//sign
+		else if($_POST['Sign'] == 'Sign in'){//sign
 	//die('!');
 	//форма входа
 			$polis = $_POST['polis'];
@@ -151,7 +154,7 @@ function check_p($reg_auth) {
 				echo "test 1";
 				$sql = "SELECT * FROM patient where polis=:polis";
 				$a = sql_quarry($sql,$dbh,'polis',$polis);
-
+                var_dump($a);
 				if ($a != 0) {
 					$res = sql_quarry($sql, $dbh, 'info', $polis);
 					if ($res == NULL) {
@@ -159,10 +162,12 @@ function check_p($reg_auth) {
 						die();
 					}
 					echo "test";
-					var_dump($res[0]['pass']);
-					var_dump($pass);
-
+                    var_dump($pass);
+                    var_dump($res[0]['pass']);
+                    var_dump(password_hash($pass,PASSWORD_BCRYPT, $options));
+                    var_dump(password_verify($pass,$res[0]['pass']));
 					if (password_verify($pass,$res[0]['pass'])) {
+					    echo "here";
 						$_SESSION['id'] = $res[0]['id'];// выдается id
 						$_SESSION['name'] = $res[0]['name'];//сессии передаётся имя пользователя для вывода его на экране
 						$_SESSION['surname'] = $res[0]['surname'];//сессии передаётся имя пользователя для вывода его на экране
@@ -179,80 +184,3 @@ function check_p($reg_auth) {
 			}
 		}
 	}
-	//		header('Location:reference.php');
-	//    include 'toolbar.php';
-	//    //var_dump($status_err);
-	//    require_once  ("area_gen.php");
-	//    $inp_login=new area_gen(1);
-	//    $inp_pass=new area_gen(1);
-	//    $inp_soap=new area_gen(1);
-	//    $inp_sub=new area_gen();
-	//
-	//    $inp_pass->set_p('type','password');
-	//    $inp_soap->set_p('type','email');
-	//    $inp_sub->set_p('type','submit');
-	//    ////var_dump($status_err['no']);
-	//    echo '<div id="au"';
-	//        echo '<ul>';
-	//            echo '<li>';
-	//                echo '<form method="post">';
-	//                    if (($_POST['action'] == 'Вход')&&!($status_err['no']['check'])) echo '<p class="wr">Заполните все поля</p>';
-	//                    echo '<h2>Вход</h2>';
-	//
-	//                    if ($status_err['no']['name']==true)  echo '<p class="wr"><b>заполните это поле</b></p>';
-	//                    elseif($status_err['user !exist']==true) echo '<p class="wr"><b>Данного пользователя не существует</b></p>';
-	//                    echo '<p> Имя пользователя(от 5 до 20 символов, только буквы латинского алфавита)</p>';
-	//                    $inp_login->make_input_echo('polis','Введите логин','[a-zA-Z0-9]{5,20}');
-	//
-	//
-	//                    if ($status_err['no']['pass']==true)  echo '<p class="wr"><b>заполните это поле</b></p>';
-	//                    elseif($status_err['wr pass']==true) echo '<p class="wr">Неверный пароль</p>';
-	//                    echo '<p>Пароль</p>';
-	//                    $inp_pass->make_input_echo('pass','Введите пароль','{8,20}');
-	//                    echo '<br><br>';
-	//
-	//                    $inp_sub->make_input_echo('action',NULL,NULL,'Вход');
-	//
-	//                echo '</form>';
-	//            echo '</li>';
-	//            echo '<li>';
-	//                echo '<form  method="post">';
-	//
-	//                    if (($_POST['action'] == 'Регистрация')&&!($status_err['no']['check'])) echo '<p class="wr">Заполните все поля</p>';
-	//                    elseif($status_err['user exist']==true) echo '<p class="wr"><b>Пользователь с данным именем уже существует</p>';
-	//                    echo '<h2>Регистрация</h2>';
-	//
-	//                    if ($status_err['no']['name_reg']==true)  echo '<p class="wr"><b>заполните это поле</b></p>';
-	//                    echo '<p> Имя пользователя(от 5 до 20 символов, только буквы латинского алфавита)</p>';
-	//                    $inp_login->make_input_echo('polis_reg','Придумайте логин','[a-zA-Z0-9]{5,20}');
-	//
-	//                    if ($status_err['no']['soap']==true)  echo '<p class="wr"><b>заполните это поле</b></p>';
-	//                    echo '<p>Введите адресс e-mail</p>';
-	//                    $inp_soap->make_input_echo('soap','Введите e-mail');
-	//
-	//
-	//                    if ($status_err['no']['pass-']==true)  echo '<p class="wr"><b>заполните это поле</b></p>';
-	//                    elseif($status_err['wr conf']==true) echo '<p class="wr"><b>Пароли не совпадают</b></p>';
-	//                    echo '<p>Пароль</p>';
-	//                    $inp_pass->make_input_echo('pass-','Придумайте пароль','{8,20}');
-	//
-	//                    if ($status_err['no']['pass+']==true)  echo '<p class="wr"><b>заполните это поле</b></p>';
-	//                    echo '<p>Подтвердите пароль</p>';
-	//                    $inp_pass->make_input_echo('pass+','Подтвердите пароль','{8,20}');
-	//                    echo   '<br><br>';
-	//
-	//                    $inp_sub->make_input_echo('action',NULL,NULL,'Регистрация');
-	//
-	//                echo '</form>';
-	//           echo '</li>';
-	//        echo '</ul>';
-	//    echo '</div>';
-	//echo '</body>';
-
-
-	        //TODO: вывод пароли не совпадают и пользователь существует одновременно done!
-	        //вывод пароли не совпадают done
-	        //исправление class wr на class --done
-	        //добавить class cor
-	        //TODO: pattern password
-	//todo:
--- registration.php
+++ registration.php
@@ -2,34 +2,41 @@
 include_once "header.php";
 ?>
 <section>
-	 <div class="wrapper">
+    <div class="wrapper">
         <div class="content">
             <h1>Регистрация нового пользователя</h1>
-            <form class="registration" action="handler.php">
+            <form class="registration" action="handler.php" method="post">
                 <div class="block">
                     <label for="">Имя</label>
-            	    <input type="" placeholder="Введите ваше имя" required><br>
-
-            	    <label for="">Фамилия</label>
-            	    <input required type="" placeholder="Введите вашу фамилию"><br>
-
-            	    <label for="">Пароль</label>
-            	    <input required type="password" placeholder="Введите пароль"><br>
-
-            	    <label for="">Номер полиса ОМС</label>
-            	    <input required type="tel" placeholder="Например, 5634 2646 6437 3462" minlength="16"
-            	    maxlength="16"><br>
-
-                    <label for="">Email</label>
-                    <input required type="email" placeholder="Введите вашу почту"><br>
-
-                    <label class="type" for="">Тип профиля</label>
-                    <input  type="tel" placeholder="Клиент = 1, Врач = 2" minlength="1" maxlength="1"><br>
-                    <input  type="hidden" name="Sign" value="Sign up">
+                    <input required type="" name="name" placeholder="Введите ваше имя" ><br>
                 </div>
-                   <button class="submit" type="submit">
-                            <span>Зарегистрироваться</span>
-                        </button>
+                <div class="block">
+                    <label for="">Фамилия</label>
+                    <input required type="" name="surname"placeholder="Введите вашу фамилию"><br>
+                </div>
+                <div class="block">
+                    <label for="">Пароль</label>
+                    <input required type="password" name="pass+" placeholder="Введите пароль"><br>
+                </div>
+                <div class="block">
+                    <label for="">Подтвердите пароль</label>
+                    <input required type="password" name="pass-" placeholder="Подтвердите пароль"><br>
+                </div>
+                <div class="block">
+                    <label for="">Номер полиса ОМС</label>
+                    <input required type="tel" name="polis_reg" placeholder="Например, 5634 2646 6437 3462" minlength="16"
+                           maxlength="16"><br></div>
+                <div class="block">
+                    <label for="">Email</label>
+                    <input required type="email" name="email" placeholder="Введите вашу почту"><br></div>
+                <div class="block">
+                    <label class="type" for="">Тип профиля</label>
+                    <input  type="tel" placeholder="Клиент = 1, Врач = 2" minlength="1"
+                            maxlength="1"><br></div>
+                <input  type="hidden" name="Sign" value="Sign up">
+                <button class="submit">
+                    <span>Зарегистрироваться</span>
+                </button>
             </form>
         </div>
     </div>
