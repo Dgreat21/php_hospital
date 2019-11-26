68e137097bfbc4451fc6f95875b9cb1fb8a24056
Mark Shamray <endeymus@gmail.com>
2019-11-26 23:11:42 +0300

    Исправленный JS

--- js/registration.js
+++ js/registration.js
@@ -8,7 +8,6 @@ function onclick_back_hndl(e)
     background.removeEventListener('click', onclick_back_hndl);
     background.remove();
 }
-
 function get_cookie ( cookie_name )
 {
     var results = document.cookie.match ( '(^|;) ?' + cookie_name + '=([^;]*)(;|$)' );
@@ -18,99 +17,25 @@ function get_cookie ( cookie_name )
     else
         return null;
 }
-
 function add_market_meta(){
-    let meta_block = document.getElementById('meta'), select, form, h1, input_l, input_p, subm;
-    let field1, field2, p_h, a_h;
-    // fragment = document.createDocumentFragment();
-
-    // var body_block = document.getElementsByName('body');
 
     let background = document.createElement("div");
     background.setAttribute("class", "login-background");
-    /*форма*/
-    form = document.createElement("form");
-    form.setAttribute('id', 'login');
-    form.setAttribute('method', 'post');
-    form.setAttribute('action', 'handler.php');
-    form.setAttribute('name', 'Вход');
-    /*Заголовок*/
-    h1 = document.createElement("h1");
-    h1.textContent = "Авторизация";
-    /*Поле fieldset1*/
-    field1 = document.createElement("fieldset");
-    field1.setAttribute('id', 'inputs');
-    /*Поле логина*/
-    input_l = document.createElement("input");
-    input_l.setAttribute('id', 'username');
-    input_l.setAttribute('type', 'text');
-    input_l.setAttribute('name', 'polis');
-    input_l.setAttribute('placeholder', 'Полис ОМС');
-    /*Поле пароля*/
-    input_p = document.createElement("input");
-    input_p.setAttribute('id', 'password');
-    input_p.setAttribute('type', 'password');
-    input_p.setAttribute('name', 'password');
-    input_p.setAttribute('placeholder', 'Пароль');
-    //поле типа пользователя
-    // select.innerHTML(  "<select name='type'>" +
-    //                 "<option selected>Пациент</option>" +
-    //                 "<option>Врач</option>" +
-    //             "</select>"
-    // );
+    background.innerHTML += "<form id='login' method='post' action='handler.php' name='Вход'>" +
+        "<h1>Авторизация</h1>" +
+        "<fieldset id='inputs'>" +
+        "<input id='username' type='text' name='polis' placeholder='ПОЛИС ОМС'>" +
+        "<input id='password' type='password' name='password' placeholder='Пароль'>" +
+        "</fieldset>"+ "<fieldset id='actions'>" +
+        "<input id='submit' type='submit' value='Войти'>" +
+        "<p id='haveno_account'>У вас нет Аккаунта?<a href='registration.php'>Зарегистрироваться</a></p>" +
+        //Вот поле я добавил
+        "<input type='hidden' name='Sign' value='Sign in'>" +
+        "</fieldset>" +
+        "</form>";
 
-    /*Поле fieldset2*/
-    field2 = document.createElement("fieldset");
-    field2.setAttribute('id', 'actions');
-    /*Кнопка вход*/
-    subm = document.createElement("input");
-    subm.id = "submit";
-    subm.type = "submit";
-    subm.value= "Войти";
-    /*нет аккича*/
-    p_h = document.createElement("p");
-    p_h.setAttribute('id', 'haveno_account');
-    p_h.textContent = "У вас нет аккаунта?";
-    a_h = document.createElement("a");
-    a_h.textContent = "Зарегистрироваться";
-    a_h.setAttribute('href', 'registration.php');
-    a_h.setAttribute('onclick', 'registration()');
-    /* Неверно введен пароль*/
-    let cho = document.createElement("cho");
-    cho.setAttribute('id', 'incor_pas');
-    cho.textContent = "Неверный пароль!";
-    /*
-    div ->
-        form ->
-            h1;
-            fieldset1 ->
-                input(user);
-                input(pass);
-            fieldset2 ->
-                input(sub);
-                p ->
-                    a;
-     */
-    field1.appendChild(input_l);
-    field1.appendChild(input_p);
-    // field1.appendChild(select);
-    let coocks = get_cookie("wr pass");
-    if (coocks)
-    {
-        field2.appendChild(cho);}
-    field2.appendChild(subm);
-    p_h.appendChild(a_h);
-    field2.appendChild(p_h);
-    form.appendChild(h1);
-    form.appendChild(field1);
-    form.appendChild(field2);
-    // fragment.appendChild(div_n);
-    background.appendChild(form);
     document.getElementsByClassName("meta-div")[0].appendChild(background);
-    // while(meta_block.firstChild){
-    //     meta_block.removeChild(meta_block.firstChild);
-    // }
-    // meta_block.appendChild(fragment);
 
-   background.addEventListener("click", onclick_back_hndl, true);
-};
+
+    background.addEventListener("click", onclick_back_hndl, true);
+}
