956ab57e2f71ffe0b747972a603de11c8dd7836e
kefs17 <57544453+kefs17@users.noreply.github.com>
2019-12-10 20:22:47 +0300

    upd

--- admin.php
+++ admin.php
@@ -19,6 +19,7 @@ require_once ("my_sql.php");
 ?>
 <section>
     <div class="wrapper">
+        <?php
     $hospitals = [
             2, 49, 94, 133
     ];
@@ -97,8 +98,11 @@ require_once ("my_sql.php");
                     echo "<form method='post' action='hand_admin.php'>
                                                 <input type='submit'  class='__r right __s html_architect' value='Добавить'>
                                         </div><br>";
+
+?>
 </div>
 </section>
+<?php
     include_once("footer.php");
 //	if($_COOKIE['id'] != 'god')
 //		header('location:https://www.google.com/search?q=%D0%BF%D0%BE%D1%87%D0%B5%D0%BC%D1%83+%D1%8F+%D0%B3%D0%B5%D0%B9+%D0%B8+%D0%B4%D0%BE%D0%BB%D0%B1%D0%B0%D0%B5%D0%B1+%D0%BB%D0%B5%D0%B7%D1%83+%D0%BA%D1%83%D0%B4%D0%B0+%D0%BD%D0%B5+%D0%BD%D0%B0%D0%B4%D0%BE%3F&oq=%D0%BF%D0%BE%D1%87%D0%B5%D0%BC%D1%83+%D1%8F+%D0%B3%D0%B5%D0%B9+%D0%B8+%D0%B4%D0%BE%D0%BB%D0%B1%D0%B0%D0%B5%D0%B1+%D0%BB%D0%B5%D0%B7%D1%83+%D0%BA%D1%83%D0%B4%D0%B0+%D0%BD%D0%B5+%D0%BD%D0%B0%D0%B4%D0%BE%3F&aqs=chrome..69i57.814j0j0&sourceid=chrome&ie=UTF-8');
